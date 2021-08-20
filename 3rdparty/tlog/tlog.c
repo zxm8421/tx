69ee586391b39067d4d7f98e46f614bb91ee4abf
♪紫小墨 <zxm8421@gmail.com>
2021-08-21 02:17:12 +0800

    tlog_hexdump优化

--- 3rdparty/tlog/tlog.c
+++ 3rdparty/tlog/tlog.c
@@ -188,6 +188,23 @@ ti tlog_rawprint(const tc *file, const ti line, const tc *func, const ti filter,
 
 ti tlog_rawhexdump(const tc *file, const ti line, const tc *func, const ti filter, const ti level, const tc *info, const void *ptr, const ti len)
 {
+	const tc hexTable[256 * 2 + 1] = "000102030405060708090A0B0C0D0E0F"
+									 "101112131415161718191A1B1C1D1E1F"
+									 "202122232425262728292A2B2C2D2E2F"
+									 "303132333435363738393A3B3C3D3E3F"
+									 "404142434445464748494A4B4C4D4E4F"
+									 "505152535455565758595A5B5C5D5E5F"
+									 "606162636465666768696A6B6C6D6E6F"
+									 "707172737475767778797A7B7C7D7E7F"
+									 "808182838485868788898A8B8C8D8E8F"
+									 "909192939495969798999A9B9C9D9E9F"
+									 "A0A1A2A3A4A5A6A7A8A9AAABACADAEAF"
+									 "B0B1B2B3B4B5B6B7B8B9BABBBCBDBEBF"
+									 "C0C1C2C3C4C5C6C7C8C9CACBCCCDCECF"
+									 "D0D1D2D3D4D5D6D7D8D9DADBDCDDDEDF"
+									 "E0E1E2E3E4E5E6E7E8E9EAEBECEDEEEF"
+									 "F0F1F2F3F4F5F6F7F8F9FAFBFCFDFEFF";
+
 	ti le = len;
 	tb *p = (tb *)ptr;
 
@@ -195,44 +212,33 @@ ti tlog_rawhexdump(const tc *file, const ti line, const tc *func, const ti filte
 
 	ti cur = 0;
 	cur += snprintf(buf + cur, 256,
-					"\n"
-					"le = %dB, %s\n"
-					"     00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n",
+					"le = %dB, %s",
 					le, info);
 
 	for (ti i = 0; i < le; i++)
 	{
-		if ((i % 16) == 0)
+		if ((i % 256) == 0)
 		{
-			cur += snprintf(buf + cur, 6,
-							"%04X ",
-							i);
+			buf[cur++] = '\n';
 		}
 
-		if (i == (le - 1))
-		{
-			cur += snprintf(buf + cur, 3,
-							"%02X",
-							p[i]);
-		}
-		else
+		buf[cur++] = hexTable[p[i] * 2];
+		buf[cur++] = hexTable[p[i] * 2 + 1];
+
+		if (i != (le - 1))
 		{
 			if ((i % 16) == 15)
 			{
-				cur += snprintf(buf + cur, 4,
-								"%02X\n",
-								p[i]);
+				buf[cur++] = '\n';
 			}
 			else
 			{
-				cur += snprintf(buf + cur, 4,
-								"%02X ",
-								p[i]);
+				buf[cur++] = ' ';
 			}
 		}
 	}
 
-	cur += snprintf(buf + cur, 2, "\n");
+	buf[cur++] = '\n';
 
 	return tlog_rawprint(file, line, func, filter, level, "%s", buf);
 }
