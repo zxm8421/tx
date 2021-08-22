5b99c485f88ddce9768b0aadaacf6fcb20c7a3c7
♪紫小墨 <zxm8421@gmail.com>
2021-08-22 11:10:24 +0800

    版本信息相关函数

--- app/version/version.cpp
+++ app/version/version.cpp
@@ -7,6 +7,40 @@
 #define TLOG_LOCAL_FILTER TLOG_D
 #include <tlog/tlog.h>
 
+// qmake 会自动生成版本信息
+#ifndef buildTime
+#pragma message("编译器未生成buildTime")
+#define buildTime 0LL
+#endif
+#ifndef buildSalt
+#define buildSalt "000000"
+#endif
+#ifndef buildBranch
+#pragma message("编译器未生成buildBranch")
+#define buildBranch "none"
+#endif
+#ifndef buildSHA1
+#pragma message("编译器未生成buildSHA1")
+#define buildSHA1 "0000000"
+#endif
+
+#ifndef buildTarget
+#pragma message("编译器未生成buildTarget")
+#define buildTarget "app"
+#endif
+
+#ifndef buildVer_Major
+#define buildVer_Major 0
+#endif
+#ifndef buildVer_Minor
+#define buildVer_Minor 0
+#endif
+#ifndef buildVer_Patch
+#define buildVer_Patch 0
+#endif
+
+#define buildVer (buildVer_Major * 1000 * 1000 + buildVer_Minor * 1000 + buildVer_Patch)
+
 ti showVer()
 {
 	QString verInfo("\n");
@@ -39,4 +73,52 @@ ti showVer()
 	tlog(TLOG_I, "%s", verInfo.toUtf8().data());
 
 	return 0;
+}
+
+QString getBuildTarget()
+{
+	return QString(buildTarget);
+}
+
+QVersionNumber getVersionNumber()
+{
+	return QVersionNumber(buildVer_Major, buildVer_Minor, buildVer_Patch);
+}
+
+QDateTime getBuildTime()
+{
+	return QDateTime::fromSecsSinceEpoch(buildTime);
+}
+
+QString getBuildSalt()
+{
+	return QString(buildSalt);
+}
+
+constexpr tu64 getBuildSalt_u64()
+{
+	const tu64 prime = 0x00000100000001B3ULL;
+	const tu64 basis = 0xCBF29CE484222325ULL;
+
+	tu64 ret = basis;
+	tc *str = (tc *)buildSalt;
+
+	while (*str)
+	{
+		ret ^= *str;
+		ret *= prime;
+		str++;
+	}
+
+	return ret;
+}
+
+QString getBuildBranch()
+{
+	return QString(buildBranch);
+}
+
+QString getBuildSHA1()
+{
+	return QString(buildSHA1);
 }
--- app/version/version.h
+++ app/version/version.h
@@ -7,39 +7,8 @@
 
 #include <tjz/ttype.h>
 
-// qmake 会自动生成版本信息
-#ifndef buildTime
-#pragma message("编译器未生成buildTime")
-#define buildTime 0LL
-#endif
-#ifndef buildSalt
-#define buildSalt "000000"
-#endif
-#ifndef buildBranch
-#pragma message("编译器未生成buildBranch")
-#define buildBranch "none"
-#endif
-#ifndef buildSHA1
-#pragma message("编译器未生成buildSHA1")
-#define buildSHA1 "0000000"
-#endif
-
-#ifndef buildTarget
-#pragma message("编译器未生成buildTarget")
-#define buildTarget "app"
-#endif
-
-#ifndef buildVer_Major
-#define buildVer_Major 0
-#endif
-#ifndef buildVer_Minor
-#define buildVer_Minor 0
-#endif
-#ifndef buildVer_Patch
-#define buildVer_Patch 0
-#endif
-
-#define buildVer (buildVer_Major * 1000 * 1000 + buildVer_Minor * 1000 + buildVer_Patch)
+#include <QVersionNumber>
+#include <QDateTime>
 
 /**
  * @brief 显示版本号以及编译信息
@@ -47,3 +16,52 @@
  * @return 
  */
 ti showVer();
+
+/**
+ * @brief 获取编译目标
+ * 
+ * @return 
+ */
+QString getBuildTarget();
+
+/**
+ * @brief 获取版本号
+ * 
+ * @return 
+ */
+QVersionNumber getVersionNumber();
+
+/**
+ * @brief 获取编译时间
+ * 
+ * @return 
+ */
+QDateTime getBuildTime();
+
+/**
+ * @brief 获取编译盐
+ * 
+ * @return 
+ */
+QString getBuildSalt();
+
+/**
+ * @brief 获取编译盐
+ * 
+ * @return 
+ */
+constexpr tu64 getBuildSalt_u64();
+
+/**
+ * @brief 获取编译分支
+ * 
+ * @return 
+ */
+QString getBuildBranch();
+
+/**
+ * @brief 获取编译commit sha1
+ * 
+ * @return 
+ */
+QString getBuildSHA1();
