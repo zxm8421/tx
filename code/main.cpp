41a3415e3c4357bef3ea47ed1fdd83a80dc5a502
♪紫小墨 <zxm8421@gmail.com>
2021-08-31 21:00:23 +0800

    去掉tsh

--- code/main.cpp
+++ code/main.cpp
@@ -10,14 +10,6 @@
 #include "version/version.h"
 #include "ui/mainwindow.h"
 
-TSH_CMD_DEFINE(tlog_test, "tlog 测试")
-{
-	printf("tsh test\n");
-	printf("tsh 测试\n");
-
-	return 0;
-}
-
 int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
 {
 	tlog_init();
@@ -28,8 +20,6 @@ int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
 
 	tlog(TLOG_I, "start ...");
 	showVer();
-
-	// tsh_init();
 
 	QApplication a(argc, argv);
 	MainWindow w;
